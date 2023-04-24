a43bda5d8bd1c4a663aa27b61fb7aa3746cd9380
ruslan <bumerboy86@gmail.com>
2023-04-24 13:02:36 +0600

    Added scrolling

--- src/containers/NotesList/NotesList.tsx
+++ src/containers/NotesList/NotesList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import styles from './NotesList.module.css';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { Note } from '../../components/Note/Note';
@@ -7,13 +7,21 @@ import { getNotes } from '../../store/note.slice';
 export const NotesList = () => {
     const { notes } = useAppSelector(state => state.note);
     const dispatch = useAppDispatch();
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container !== null) {
+          container.scrollTop = container.scrollHeight;
+        }
+    }, [ notes ])
 
     useEffect(() => {
         dispatch(getNotes());
     }, [])
 
     return (
-        <div className={styles.NotesList}>
+        <div className={styles.NotesList} ref={containerRef}>
             {notes.length && notes.map(item => {
                 return <Note key={item.id} note={item}/>
             })}
